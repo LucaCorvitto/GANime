0487fe1247a23d58c7e4a155e2ad5bb7281e358f
LucaCorvitto <87773518+LucaCorvitto@users.noreply.github.com>
2022-07-04 17:52:35 +0200

    Add files via upload

---
+++ utils/utils_sr.py
@@ -0,0 +1,132 @@
+import numpy as np
+import torch
+
+import os
+from os import listdir
+from os.path import join
+
+from PIL import Image
+
+import torch.utils.data
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+
+import torchvision.utils as utils
+from torchvision.transforms import Compose, RandomCrop, ToTensor, ToPILImage, CenterCrop, Resize, Normalize
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
+
+def calculate_valid_crop_size(crop_size, upscale_factor):
+    return crop_size - (crop_size % upscale_factor)
+
+def to_image():
+    return Compose([
+        ToPILImage(),
+        ToTensor()
+    ])
+	
+class TrainDataset(Dataset):
+    def __init__(self, dataset_dir, crop_size, upscale_factor):
+        super(TrainDataset, self).__init__()
+        self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
+        crop_size = calculate_valid_crop_size(crop_size, upscale_factor)
+        self.hr_preprocess = Compose([CenterCrop(512), RandomCrop(crop_size), ToTensor()]) #from 384 to 512
+        self.lr_preprocess = Compose([ToPILImage(), Resize(crop_size // upscale_factor, interpolation=Image.BICUBIC), ToTensor()])
+
+    def __getitem__(self, index):
+        hr_image = self.hr_preprocess(Image.open(self.image_filenames[index]))
+        lr_image = self.lr_preprocess(hr_image)
+        return lr_image, hr_image
+
+    def __len__(self):
+        return len(self.image_filenames)
+        
+class DevDataset(Dataset):
+	def __init__(self, dataset_dir, upscale_factor):
+		super(DevDataset, self).__init__()
+		self.upscale_factor = upscale_factor
+		self.image_filenames = [join(dataset_dir, x) for x in listdir(dataset_dir) if is_image_file(x)]
+
+	def __getitem__(self, index):
+		hr_image = Image.open(self.image_filenames[index])
+		crop_size = calculate_valid_crop_size(512, self.upscale_factor) #from 128 to 512
+		lr_scale = Resize(crop_size // self.upscale_factor, interpolation=Image.BICUBIC)
+		hr_scale = Resize(crop_size, interpolation=Image.BICUBIC)
+		hr_image = CenterCrop(crop_size)(hr_image)
+		lr_image = lr_scale(hr_image)
+		hr_restore_img = hr_scale(lr_image)
+		norm = ToTensor()
+		return norm(lr_image), norm(hr_restore_img), norm(hr_image)
+
+	def __len__(self):
+		return len(self.image_filenames)
+
+def print_first_parameter(net):	
+	for name, param in net.named_parameters():
+		if param.requires_grad:
+			print (str(name) + ':' + str(param.data[0]))
+			return
+
+def check_grads(model, model_name):
+	grads = []
+	for p in model.parameters():
+		if not p.grad is None:
+			grads.append(float(p.grad.mean()))
+
+	grads = np.array(grads)
+	if grads.any() and grads.mean() > 100:
+		print('WARNING!' + model_name + ' gradients mean is over 100.')
+		return False
+	if grads.any() and grads.max() > 100:
+		print('WARNING!' + model_name + ' gradients max is over 100.')
+		return False
+		
+	return True
+
+def get_grads_D(net):
+	top = 0
+	bottom = 0
+	for name, param in net.named_parameters():
+		if param.requires_grad:
+			# Hardcoded param name, subject to change of the network
+			if name == 'net.0.weight':
+				top = param.grad.abs().mean()
+				#print (name + str(param.grad))
+			# Hardcoded param name, subject to change of the network
+			if name == 'net.26.weight':
+				bottom = param.grad.abs().mean()
+				#print (name + str(param.grad))
+	return top, bottom
+	
+def get_grads_D_WAN(net):
+	top = 0
+	bottom = 0
+	for name, param in net.named_parameters():
+		if param.requires_grad:
+			# Hardcoded param name, subject to change of the network
+			if name == 'net.0.weight':
+				top = param.grad.abs().mean()
+				#print (name + str(param.grad))
+			# Hardcoded param name, subject to change of the network
+			if name == 'net.19.weight':
+				bottom = param.grad.abs().mean()
+				#print (name + str(param.grad))
+	return top, bottom
+
+def get_grads_G(net):
+	top = 0
+	bottom = 0
+	#torch.set_printoptions(precision=10)
+	#torch.set_printoptions(threshold=50000)
+	for name, param in net.named_parameters():
+		if param.requires_grad:
+			# Hardcoded param name, subject to change of the network
+			if name == 'conv1.0.weight':
+				top = param.grad.abs().mean()
+				#print (name + str(param.grad))
+			# Hardcoded param name, subject to change of the network
+			if name == 'upsample.2.weight':
+				bottom = param.grad.abs().mean()
+				#print (name + str(param.grad))
+	return top, bottom
